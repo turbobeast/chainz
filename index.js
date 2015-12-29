d05852814f8eb0b3a33cf014b74de0c8b878bcea
george michael faust <georgemichaelfaust@gmail.com>
2015-12-29 00:53:48 -0500

    passing values to next function -- es2015 - call next async - added tests - version bump 1.0

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-var ChainGang = function (gang) {
-
-  var next = function () {
+const chainz = (...gangs) => {
+  let gang = [];
+  const next = function (...args) {
     if(gang.length > 0) {
-      gang.shift()();
+      gang.shift()(...args);
     }
   };
 
-  function Chain (func) {
+  const Chain = func => {
     if(typeof func !== 'function') {
-      console.warn('every element in array, must be a function');
+      console.warn('every argument must be a function');
       func = next;
     }
-    return function () {
-      var args = Array.prototype.slice.call(arguments);
+    return function (...args) {
       args.push(next);
-      func.apply(null, args);
+      // run next event loop
+      setTimeout(func.bind(Object.create(null), ...args), 0);
     };
-  }
+  };
 
-  var gang = gang.map(Chain);
+  gang = gangs.map(Chain);
   next();
-
 };
 
-module.exports = ChainGang;
+module.exports = chainz;
